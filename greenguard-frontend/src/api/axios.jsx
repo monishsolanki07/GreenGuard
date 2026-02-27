0e2c929152776ea03420c587e5a7415edd91d498
monish <monishsolanki100@gmail.com>
2026-02-27 13:22:41 +0530

    update frontend API base URL for production

--- greenguard-frontend/src/api/axios.jsx
+++ greenguard-frontend/src/api/axios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000/api/",
+  baseURL: "https://greenguard-backend-m87i.onrender.com/api/",
 });
 
 api.interceptors.request.use((config) => {
