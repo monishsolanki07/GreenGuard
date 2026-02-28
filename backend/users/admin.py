4454d2f701b32f21c40d29e90e16a7a6b0e95160
monish <monishsolanki100@gmail.com>
2026-02-28 19:55:18 +0530

    refactor(admin): simplify UserAdmin registration and comment out custom fields

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -3,35 +3,35 @@ from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from .models import User
 
 
-@admin.register(User)
-class UserAdmin(BaseUserAdmin):
-    model = User
+admin.site.register(User)
+# class UserAdmin(BaseUserAdmin):
+#     model = User
 
-    list_display = (
-        "username",
-        "email",
-        "role",
-        "company_name",
-        "is_staff",
-        "is_superuser",
-        "is_active",
-    )
+#     list_display = (
+#         "username",
+#         "email",
+#         "role",
+#         "company_name",
+#         "is_staff",
+#         "is_superuser",
+#         "is_active",
+#     )
 
-    list_filter = (
-        "role",
-        "is_staff",
-        "is_superuser",
-        "is_active",
-    )
+#     list_filter = (
+#         "role",
+#         "is_staff",
+#         "is_superuser",
+#         "is_active",
+#     )
 
-    fieldsets = BaseUserAdmin.fieldsets + (
-        ("Custom Fields", {
-            "fields": ("role", "company_name"),
-        }),
-    )
+#     fieldsets = BaseUserAdmin.fieldsets + (
+#         ("Custom Fields", {
+#             "fields": ("role", "company_name"),
+#         }),
+#     )
 
-    add_fieldsets = BaseUserAdmin.add_fieldsets + (
-        ("Custom Fields", {
-            "fields": ("role", "company_name"),
-        }),
-    )
+#     add_fieldsets = BaseUserAdmin.add_fieldsets + (
+#         ("Custom Fields", {
+#             "fields": ("role", "company_name"),
+#         }),
+#     )
